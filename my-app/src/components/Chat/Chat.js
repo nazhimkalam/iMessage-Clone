fe096f956358a4d57f558d97163400144c8feef8
Nazhim <nazhimkalam@gmail.com>
2020-10-17 15:41:18 +0530

    completed the message footer section

--- my-app/src/components/Chat/Chat.js
+++ my-app/src/components/Chat/Chat.js
@@ -1,18 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Chat.css';
+import MicNoneIcon from '@material-ui/icons/MicNone';
+import { IconButton } from '@material-ui/core';
 
 function Chat() {
+	const [input, setInput] = useState('');
+
+	// fires this function when send button clicked
+	const sendMessage = (e) => {
+		e.preventDefault();
+
+		// firebase stuff
+
+		setInput('');
+	};
+
 	return (
 		<div className="chat">
-			{/* Header user*/}
 			<div className="chat__header">
 				<h4>
 					To: <span className="chat__name">Channel name</span>
 				</h4>
 				<strong>Details</strong>
 			</div>
-			{/* body txt */}
-			{/* footer input */}
+
+            <div className="chat__messages">
+                <h2>I am a message</h2>
+                <h2>I am a message</h2>
+                <h2>I am a message</h2>
+                <h2>I am a message</h2>
+            </div>
+
+
+			<div className="chat__input">
+				<form>
+					<input type="text" placeholder="iMessage" value={input} onChange={(e) => setInput(e.target.value)} />
+					<button onClick={sendMessage}>Send Message</button>
+				</form>
+				<IconButton>
+					<MicNoneIcon />
+				</IconButton>
+			</div>
 		</div>
 	);
 }
