fddf6d03dcf1ebdbfda924e4942c69fa7af7f741
Nazhim <nazhimkalam@gmail.com>
2020-10-17 15:46:58 +0530

    setting up the counter slice

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import IMessage from './components/IMessage/IMessage';
 
 function App() {
+
 	return (
 		<div className="app">
 			<IMessage />
 		</div>
-	);
+  );
+  
 }
 
 export default App;
