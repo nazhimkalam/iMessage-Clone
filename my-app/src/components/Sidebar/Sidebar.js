6313d7ee2857ff80faf8556f4ebebccae65c5de3
Nazhim <nazhimkalam@gmail.com>
2020-10-17 13:51:57 +0530

    completed the sidebar header part

--- my-app/src/components/Sidebar/Sidebar.js
+++ my-app/src/components/Sidebar/Sidebar.js
@@ -1,19 +1,28 @@
-import React from 'react'
-import './Sidebar.css'
+import React from 'react';
+import './Sidebar.css';
+import { Avatar, IconButton } from '@material-ui/core';
+import SearchIcon from '@material-ui/icons/Search';
+import RateReviewOutlinedIcon from '@material-ui/icons/RateReviewOutlined';
 
 function Sidebar() {
-    return (
-        <div>
-          <div className="sidebar__header">
-            
-          </div>
+	return (
+		<div className="sidebar">
+			<div className="sidebar__header">
+				<Avatar className="sidebar__avatar"/>
+				<div className="sidebar__input">
+					<SearchIcon />
+					<input type="text" placeholder="Search" />
+				</div>
+				<IconButton className="sidebar__inputButton" variant="outlined">
+					<RateReviewOutlinedIcon />
+				</IconButton>
+			</div>
 
-          <div className="sidebar__chat">
-            
-          </div>
-        </div>
-    )
+			<div className="sidebar__chat">
+        
+      </div>
+		</div>
+	);
 }
 
-export default Sidebar
-
+export default Sidebar;
